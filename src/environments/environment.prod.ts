1787c5d04083e905516b6ea2b297549a7f5e03f0
Luiz Gustavo <42076223+Guuh86@users.noreply.github.com>
2022-07-15 14:09:44 -0300

    Update environment.prod.ts
    removendo apis do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDskjF42jm5faLy9IAsFDs_cgBW8zAU8WI",
-    authDomain: "justdancechampions.firebaseapp.com",
-    databaseURL: "https://justdancechampions-default-rtdb.firebaseio.com",
-    projectId: "justdancechampions",
-    storageBucket: "justdancechampions.appspot.com",
-    messagingSenderId: "774689125607",
-    appId: "1:774689125607:web:5c3ccbb2b511445c4ede42",
-    measurementId: "G-XJ2384C2Q9"
-  }
 };
